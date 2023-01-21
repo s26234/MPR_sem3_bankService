bffba4cb0d34180134a9312044c774b3e3dc07a8
s26234-pj <s26234@pjwstk.edu.pl>
2023-01-21 14:16:07 +0100

    koniec 14:15

--- src/test/java/s26234Bank/mja/BankServicesMockTests.java
+++ src/test/java/s26234Bank/mja/BankServicesMockTests.java
@@ -1,4 +1,30 @@
 package s26234Bank.mja;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class BankServicesMockTests {
+    @Mock
+    private BankStorage bankStorage;
+    @Mock
+    private TransferStorage transferStorage;
+    @Mock
+    private User user;
+
+    @InjectMocks
+    private BankService bankService;
+
+
+    @Test
+    void shouldNotFindMoney() {
+        when(moneyStorage.findMoney(any())).thenReturn(null);
+        assertThat(bankService.getAllMoney()).isNull();
 }
