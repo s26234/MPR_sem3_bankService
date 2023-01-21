23ff71581628b430283c8a160c560f0da88bc222
s26234-pj <s26234@pjwstk.edu.pl>
2023-01-21 12:26:27 +0100

    Initial commit

---
+++ src/main/java/s26234Bank/mja/S26234BankApplication.java
@@ -0,0 +1,13 @@
+package s26234Bank.mja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26234BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S26234BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s26234Bank/mja/S26234BankApplicationTests.java
@@ -0,0 +1,13 @@
+package s26234Bank.mja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26234BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
