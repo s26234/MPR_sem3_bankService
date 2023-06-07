9ee6a26ddcd12ba506fbfa10bc78dd3f5967cc71
s26234-pj <s26234@pjwstk.edu.pl>
2023-06-07 19:09:15 +0200

    test revision

--- src/test/java/s26234Bank/mja/BankServiceTests.java
+++ src/test/java/s26234Bank/mja/BankServiceTests.java
@@ -2,9 +2,54 @@ package s26234Bank.mja;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class BankServiceTests {
 
+    private BankService bankService = new BankService(new ClientStorage(), new BankStorage());
+
+    @Test
+    void ifThereIsChangesInStatus() {
+        Client client = new Client("Karolina", "Koralowa");
+        double saldo = 500.00;
+        String status = "ACCEPTED";
+        int id = 2;
+
+        bankService.registerUser(client, id, status, (int) saldo);
+        String status1 = bankService.getBankerStatusOfId(id);
+        bankService.setBankerStatusOfId(id, "DECLINED");
+        String status2 = bankService.getBankerStatusOfId(id);
+        assertThat(status1).isNotEqualTo(status2);
+    }
+
+        @Test
+        void IfThereIsChangesInSaldoAfterValidCashout() {
+            Client client = new Client("Adam", "Koralowy");
+            double saldo = 800.00;
+            String status = "ACCEPTED";
+            int id = 3;
+
+            bankService.registerUser(client,id,status,(int) saldo);
+            double saldo1 = bankService.getBankerSaldoOfId(id);
+            bankService.removeSaldoOfId(id,200);
+            double saldo2 = bankService.getBankerSaldoOfId(id);
+            assertThat(saldo1).isNotEqualTo(saldo2);
+        }
 
 
-}
+
+
+
+    }
+
+
+
+
+
+
+
+
+
--- src/test/java/s26234Bank/mja/BankServicesMockTests.java
+++ src/test/java/s26234Bank/mja/BankServicesMockTests.java
@@ -6,25 +6,39 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BankServicesMockTests {
-//    @Mock
-//    private BankStorage bankStorage;
-//    @Mock
-//    private TransferStorage transferStorage;
-//    @Mock
-//    private Client client;
-//
-//    @InjectMocks
-//    private BankService bankService;
-//
-//
-//    @Test
-//    void shouldNotFindMoney() {
-//        when(moneyStorage.findMoney(any())).thenReturn(null);
-//        assertThat(bankService.getAllMoney()).isNull();
+    @Mock
+    private BankStorage bankStorage;
+
+    @Mock
+    private ClientStorage clientStorage;
+
+    @InjectMocks
+    private BankService bankService;
+
+
+    @Test
+    void ifThereIsClientMock() {
+        Client client = new Client("Jan", "Pośpieszny");
+        when(clientStorage.getClientList()).thenReturn(List.of(client));
+        assertThat(bankService.getAllClients()).isNotNull();
+    }
+
+    @Test
+    void IfThereIsBankerMock(){
+        Client client = new Client("Dorota", "Kowalska");
+        Banker banker = new Banker(client,2,"ACCEPTED", 1200);
+        when(bankStorage.getBankerList()).thenReturn(List.of(banker));
+        assertThat(bankService.getAllBankers()).isNotNull();
+    }
+
+
+
 }
